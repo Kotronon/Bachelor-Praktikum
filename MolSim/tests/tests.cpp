cc614278d3b3bffdae3ed801d1fee9056e0d2b3e
kathi <kathi@mail.com>
2023-11-09 17:17:47 +0100

    ctest works now

--- MolSim/tests/tests.cpp
+++ MolSim/tests/tests.cpp
@@ -3,11 +3,13 @@
 
 
 //@TODO write tests with TEST()
+//To compile tests write cmake --build . in terminal and afterwarts ctest works
 
 TEST(HELLOTEST, qualitycheck){
         EXPECT_EQ(7 * 6, 42);
 }
 
 int main(){
+    testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
