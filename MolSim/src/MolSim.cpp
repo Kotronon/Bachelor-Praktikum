25f299a0c01716f06fd959d9d52540c46e3b5347
kathi <kathi@mail.com>
2023-11-11 11:27:58 +0100

    added spdlog examples and set_log_level in cmake

--- MolSim/src/MolSim.cpp
+++ MolSim/src/MolSim.cpp
@@ -48,8 +48,26 @@ double delta_t = 0.014;
 ParticleContainer container = ParticleContainer();
 
 int main(int argc, char *argsv[]) {
+    spdlog::info("Welcome to spdlog!");
+    spdlog::error("Some error message with arg: {}", 1);
 
-    std::cout << "Hello from MolSim for PSE!" << std::endl;
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+    spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    spdlog::info("{:<30}", "left aligned");
+
+    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+    spdlog::debug("This message should be displayed..");
+
+    // change log pattern
+    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+    // Compile time log levels
+    // define SPDLOG_ACTIVE_LEVEL to desired level
+    SPDLOG_TRACE("Some trace message with param {}", 42);
+    SPDLOG_DEBUG("Some debug message");
+   /* std::cout << "Hello from MolSim for PSE!" << std::endl;
     if (argc <= 2 || argc >= 8) {
         std::cout << "Erroneous programme call! " << std::endl;
         std::cout << "./MolSim <filepath/filename> [options]"<< std::endl;
@@ -102,7 +120,7 @@ int main(int argc, char *argsv[]) {
     }
 
 
-    std::cout << "output written. Terminating..." << std::endl;
+    std::cout << "output written. Terminating..." << std::endl;*/
     return 0;
 }
 
