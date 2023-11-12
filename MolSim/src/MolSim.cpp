8a0afdb83d957df4e6f2407aa6281ed26396f577
Maracondaa <anna.len.mueller@gmail.com>
2023-11-12 01:52:20 +0100

    Used ArrayUtils vector function for calculation of x,v,F

--- MolSim/src/MolSim.cpp
+++ MolSim/src/MolSim.cpp
@@ -110,26 +110,8 @@ void calculateF() {
     for (auto &p1: container) {
         force = {0., 0., 0.};
         for (auto &p2: container) {
-            if (p1 == p2) {}
-            else {
-                //Fi = SUM Fij
-                //Fij = (MiMj * (xj-xi))/(||xi-xj||2^3)
-                double mass = p1.getM() * p2.getM();
-                std::array<double, 3> diffvec{};
-                std::array<double, 3> x1 = p1.getX();
-                std::array<double, 3> x2 = p2.getX();
-                //calculating xj - xi
-                diffvec[0] = x2[0] - x1[0];
-                diffvec[1] = x2[1] - x1[1];
-                diffvec[2] = x2[2] - x1[2];
-                //calculating ||xi - xj||2
-                double before_sqrt = diffvec[0] * diffvec[0] + diffvec[1] * diffvec[1] + diffvec[2] * diffvec[2];
-                double length_dif = sqrt(before_sqrt);
-                double length_pow = pow(length_dif, 3);
-                //calculating old force + (MiMj * (xj-xi))/(||xi-xj||2^3)
-                force[0] += diffvec[0] * (mass / length_pow);
-                force[1] += diffvec[1] * (mass / length_pow);
-                force[2] += diffvec[2] * (mass / length_pow);
+            if (!(p1 == p2)) {
+                force = force + (((p1.getM() * p2.getM()) / pow((ArrayUtils::L2Norm(p1.getX() - p2.getX())), 3)) * (p2.getX() - p1.getX()));
             }
         }
         p1.setOldF(p1.getF());
@@ -141,33 +123,14 @@ void calculateF() {
 void calculateX() {
     for (auto &p: container) {
         //xi(tn+1) = xi(tn) + ∆t * vi(tn) + (∆t)^2 * Fi(tn) /2mi
-        std::array<double, 3> x_old = p.getX();
-        std::array<double, 3> v = p.getV();
-        std::array<double, 3> f = p.getF();
-        //calculating (∆t)^2 /2mi
-        double t_mul_m = delta_t * delta_t / (2 * p.getM());
-        std::array<double, 3> x_new{};
-        //calculating xi(tn) + ∆t * vi(tn) + (∆t)^2 * Fi(tn) /2mi
-        x_new[0] = x_old[0] + delta_t * v[0] + t_mul_m * f[0];
-        x_new[1] = x_old[1] + delta_t * v[1] + t_mul_m * f[1];
-        x_new[2] = x_old[2] + delta_t * v[2] + t_mul_m * f[2];
-        p.setX(x_new);
+        p.setX(p.getX() + (delta_t * p.getV()) + ((delta_t*delta_t)/(2*p.getM())) * p.getF());
     }
 }
 
 void calculateV() {
     for (auto &p: container) {
         //vi (tn+1) = vi(tn) + ∆t * Fi(tn) + Fi(tn+1) / 2mi
-        std::array<double, 3> v_old = p.getV();
-        std::array<double, 3> f = p.getF();
-        std::array<double, 3> f_old = p.getOldF();
-        std::array<double, 3> v_new{};
-        //calculating vi(tn) + ∆t * Fi(tn) + Fi(tn+1) / 2mi
-        v_new[0] = v_old[0] + (delta_t / (2 * p.getM())) * (f_old[0] + f[0]);
-        v_new[1] = v_old[1] + (delta_t / (2 * p.getM())) * (f_old[1] + f[1]);
-        v_new[2] = v_old[2] + (delta_t / (2 * p.getM())) * (f_old[2] + f[2]);
-        p.setV(v_new);
-
+        p.setV(p.getV() + ((delta_t / (2 * p.getM())) * (p.getOldF() + p.getF())));
     }
 }
 
