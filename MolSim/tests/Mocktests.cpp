12f00fe6c8f3846febd83215df7672df6b2b7316
kathi <kathi@mail.com>
2023-11-14 17:24:12 +0100

    unit tests and mock tests

--- MolSim/tests/Mocktests.cpp
+++ MolSim/tests/Mocktests.cpp
@@ -1,11 +1,8 @@
 
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
-#include "../src/ParticleContainer.h"
-#include "../src/PositionCalculator.h"
-#include "../src/VelocityCalculator.h"
-#include "../src/ForceCalculator.h"
-#include "../src/Particle.h"
+#include "mockClass.h"
+
 
 
 
@@ -14,9 +11,10 @@
 using ::testing::Mock;
 
 TEST(ParticleContainerTest, ParticleContainer){
-    ParticleContainer particles;
+    MockParticleContainer particles;
     //test addParticle
-    Particle particle({0, 0, 0}, {0, 3, 0}, 50, 0);
+    MockParticle particle;
+    particle.Particle({0, 0, 0}, {0, 3, 0}, 50, 0);
     particles.addParticle({0, 0, 0}, {0, 3, 0}, 50, 0);
     //test begin
     ASSERT_TRUE(particles.begin().base()->operator==(particle));
@@ -29,10 +27,11 @@ TEST(ParticleContainerTest, ParticleContainer){
 /*TEST(PositionTest, stroemerVelvet){
     //Checking the correctness of the stoemer velvet position calculation
     //for force = 0
-    ParticleContainer particles;
+    ::testing::NiceMock<MockParticleContainer> particles;
+    //MockParticleContainer particles;
     particles.addParticle({0, 0, 0}, {0, 3, 0}, 50, 0);
     std::array<double, 3> res = {0, 0.042, 0};
-    PositionCalculator calculator;
+    MockPositionCalculator calculator;
     calculator.PositionStoermerVerlet(particles, 0.014);
     std::vector<Particle>::iterator particleVector =  particles.begin();
     EXPECT_EQ(res, particleVector.base()->getX());
@@ -41,29 +40,29 @@ TEST(ParticleContainerTest, ParticleContainer){
 
 TEST(VelocityTest, BrownianMotionInitialization){
     //Checking the correctness of the Brownian Motion Initialization velocity calculation
-    ParticleContainer particles;
+    MockParticleContainer particles;
     particles.addParticle({0, 0, 0}, {0, 3, 0}, 50, 0);
 }
 
 TEST(VelocityTest, stroemerVelvet) {
     //Checking the correctness of the stoemer velvet velocity calculation
     //for force = 0
-    ParticleContainer particles;
+    MockParticleContainer particles;
     particles.addParticle({0, 0, 0}, {0, 3, 0}, 50, 0);
     std::array<double, 3> res = {0, 3, 0};
-    VelocityCalculator calculator;
+    MockVelocityCalculator calculator;
     calculator.VelocityStoermerVerlet(particles, 0.014);
     std::vector<Particle>::iterator particleVector = particles.begin();
     EXPECT_EQ(res, particleVector.base()->getV());
 }
 
 TEST(ForceTest, SimpleForceCalculation){
-    ParticleContainer particles;
+    MockParticleContainer particles;
     particles.addParticle({0, 0, 0}, {0, 3, 0}, 50, 0);
     particles.addParticle({0, 3, 0}, {4, 3, 5}, 20, 0);
     std::array<double, 3> res1 = {0, 1000, 0};
     std::array<double, 3> res2 = {0, -1000, 0};
-    ForceCalculator calculator;
+    MockForceCalculator calculator;
     calculator.SimpleForceCalculation(particles);
     std::vector<Particle>::iterator particleVector = particles.begin();
     EXPECT_EQ(res1, particleVector.base()->getF());
@@ -71,12 +70,12 @@ TEST(ForceTest, SimpleForceCalculation){
 }
 
 TEST(ForceTest, LennardJonesForce){
-    ParticleContainer particles;
+    MockParticleContainer particles;
     particles.addParticle({0, 0, 0}, {0, 3, 0}, 50, 0);
     particles.addParticle({0, 3, 0}, {4, 3, 5}, 20, 0);
     std::array<double, 3> res1 = {0, -8/531441, 0};
     std::array<double, 3> res2 = {0, 8/531441, 0};
-    ForceCalculator calculator;
+    MockForceCalculator calculator;
     calculator.LennardJonesForce(particles, 1, 1);
     std::vector<Particle>::iterator particleVector = particles.begin();
     EXPECT_EQ(res1, particleVector.base()->getF());
--- MolSim/tests/mockClass.h
+++ MolSim/tests/mockClass.h
@@ -5,30 +5,45 @@
 #include "../src/PositionCalculator.h"
 #include "../src/VelocityCalculator.h"
 #include "../src/ForceCalculator.h"
+#include "../src/Particle.h"
 #include "gmock/gmock.h"
 
+namespace {
 
-class MockParticleContainer: public ParticleContainer {
-public:
-    MOCK_METHOD(void , addPartocle, (const Particle &particle));
-    MOCK_METHOD((std::vector<Particle>::iterator) , begin, ());
-    MOCK_METHOD((std::vector<Particle>::iterator) , end, ());
-    MOCK_METHOD(int , size, ());
-};
+    class MockParticle : public Particle{
+    public:
+        MOCK_METHOD(void, Particle, ((std::array<double, 3>) x_arg, (std::array<double, 3>) v_arg, double m_arg, int type_arg), ());
+        MOCK_METHOD((std::array<double, 3>),getX, (), (const));
+        MOCK_METHOD((std::array<double, 3>),getV, (), (const));
+        MOCK_METHOD((std::array<double, 3>),getF, (), (const));
+        //MOCK_METHOD(bool, (operator==), (Particle & other), ());
+    };
 
-class MockForceCalculator: public  ForceCalculator {
-public:
-    MOCK_METHOD(void, SimpleForceCalculation, (ParticleContainer &container));
-    MOCK_METHOD(void, LennardJonesForce, (ParticleContainer &container, double eps, double sig));
-};
+    class MockParticleContainer : public ParticleContainer {
+    public:
+        MOCK_METHOD(void, addPartocle,((std::array<double, 3>) x_arg, (std::array<double, 3>) v_arg, double m_arg, int type_arg), ());
+        MOCK_METHOD((std::vector<Particle>::iterator), begin,(), ());
+        MOCK_METHOD((std::vector<Particle>::iterator), end, (), ());
+        MOCK_METHOD(int, size,(), ());
+    };
 
-class MockPositionCalculator: public  PositionCalculator {
-public:
-    MOCK_METHOD(void, PositionStoermerVerlet, (ParticleContainer &container, double delta_t));
-};
+    class MockForceCalculator : public ForceCalculator {
+    public:
+        MOCK_METHOD(void, SimpleForceCalculation,(ParticleContainer &container), ());
 
-class MockVelocityCalculator: public VelocityCalculator {
-public:
-    MOCK_METHOD(void, BrownianMotionInitialization, (ParticleContainer &container, double avg_v));
-    MOCK_METHOD(void, VelocityStoermerVerlet, (ParticleContainer &container, double delta_t));
-};
+        MOCK_METHOD(void, LennardJonesForce,(ParticleContainer &container, double eps, double sig), ());
+    };
+
+    class MockPositionCalculator : public PositionCalculator {
+    public:
+        MOCK_METHOD(void, PositionStoermerVerlet,(ParticleContainer &container, double delta_t),());
+    };
+
+    class MockVelocityCalculator : public VelocityCalculator {
+    public:
+        MOCK_METHOD(void, BrownianMotionInitialization,(ParticleContainer &container, double avg_v),());
+
+        MOCK_METHOD(void, VelocityStoermerVerlet,(ParticleContainer &container, double delta_t),());
+    };
+
+}
