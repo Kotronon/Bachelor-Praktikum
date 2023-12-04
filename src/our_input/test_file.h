05d9fd757af9391d94a225f538862a23a8fd27a3
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2023-12-04 18:45:11 +0100

    oops forgot .h file

---
+++ src/our_input/test_file.h
@@ -0,0 +1,18 @@
+//
+// Created by Admin on 03.12.2023.
+//
+
+#ifndef PSEMOLDYN_GROUPH_TEST_FILE_H
+#define PSEMOLDYN_GROUPH_TEST_FILE_H
+
+
+class test_file {
+
+    void setup();
+    int main();
+
+};
+
+
+
+#endif //PSEMOLDYN_GROUPH_TEST_FILE_H
