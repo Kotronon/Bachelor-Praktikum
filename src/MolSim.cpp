cd0704ab6243f8d8e84ee6409bcc0117483725e7
Maracondaa <anna.len.mueller@gmail.com>
2024-01-24 14:27:49 +0100

    example simulation

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -20,21 +20,21 @@ void plotParticlesInCells(int iteration, LinkedCellContainer &cells);
 
 //Hardcoded values for now:
 constexpr double start_time = 0;
-double end_time = 25;
+double end_time = 100;
 double delta_t = 0.0005;
 
-int dim = 2;
+int dim = 3;
 double Grav = -12.44;
 
 //if you want to use directSum please use DBL_MAX for each direction
-std::array<double, 3> domain_size = {50, 50, 1};
+std::array<double, 3> domain_size = {60, 60, 60};
 //if you want to use directSum please use DBL_MAX
-double cutoff = 2.5 * 1.0;
+double cutoff = 3.0 * 1.2;
 
 //boundary order:  left, right, up, down, behind, before
 //boundary types: "o"(outflow), "r"(reflective), "p"(periodic)
 //(if you want use directSum please use {"o", "o", "o", "o", "o", "o"})
-std::array<std::basic_string<char>, 6> boundary = {"p", "p", "r", "r", "o", "o"};
+std::array<std::basic_string<char>, 6> boundary = {"p", "p", "p", "p", "p", "p"};
 
 //input file (file will be used if valid path is given and file is not empty)
 std::string inputFile = "";
@@ -45,16 +45,16 @@ int num_checkpoints = 1;
 //path to folder to be used for output of checkpoint files
 std::string outputDirectory = "../output";
 
-double initTemperature = 50;
+double initTemperature = 40;
 int nThermostat = 1000;
 bool applyBrownianMotion = true;
 
 //optional:
-bool targetTemperatureExists = true;
+bool targetTemperatureExists = false;
 double targetTemperature = 0;
 
 //optional:
-bool differenceTemperatureExists = true;
+bool differenceTemperatureExists = false;
 double differenceTemperature = 10;
 
 //Cuboids/Disks have to be created manually in main
@@ -72,6 +72,7 @@ int main(int argc, char *argsv[]) {
         FileReader::readFile(container, inputFile.data());
         cells.addContainer(container);
     }
+
     int checkpoint = 1;
     if (num_checkpoints < 0) checkpointing = false;
     int steps_between_checkpoints = int(end_time / delta_t) / num_checkpoints;
@@ -79,8 +80,8 @@ int main(int argc, char *argsv[]) {
     //Creation of cuboids/disks for simulation with linked-cell container
     //Use either ParticleGenerator::createCuboidInCells or ParticleGenerator::createDiskInCells
 
-    //ParticleGenerator::createCuboidInCells({0.6, 2, 0}, {0, 0, 0}, {50, 14, 1}, 1.2, 1.0, cells, 1.0, 1, 1);
-    //ParticleGenerator::createCuboidInCells({0.6, 19, 0}, {0, 0, 0}, {50, 14, 1}, 1.2, 2.0, cells, 0.9412, 1, 2);
+    ParticleGenerator::createCuboidInCells({0.6, 0.6, 0.6}, {0, 0, 0}, {50, 20, 50}, 1.2, 1.0, cells, 1.2, 1, 1);
+    ParticleGenerator::createCuboidInCells({0.6, 24.0, 0.6}, {0, 0, 0}, {50, 20, 50}, 1.2, 2.0, cells, 1.1, 1, 2);
 
     double current_time = start_time;
     int iteration = 0;
@@ -128,7 +129,7 @@ int main(int argc, char *argsv[]) {
         if (iteration % 10 == 0) {
             plotParticlesInCells(iteration, cells);
         }
-        if (iteration % 100 == 0) {
+        if (iteration % 10 == 0) {
             spdlog::info("Iteration " + std::to_string(iteration) + " finished.");
         }
         if (iteration % steps_between_checkpoints == 0 && checkpointing) {
