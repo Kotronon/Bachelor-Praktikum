5dedde8706a0a89fbc699f0eb22e8e661f1f2630
Maracondaa <anna.len.mueller@gmail.com>
2024-02-01 03:50:53 +0100

    Updated parts of README

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -281,7 +281,6 @@ ASSERT_TRUE(std::abs(Thermostat::calculateCurrentTemperature(2, cells) - 10) < 0
 }
 
 int main() {
-   // testing::InitGoogleTest();
-   // return RUN_ALL_TESTS();
-    // return 0;
+   testing::InitGoogleTest();
+   return RUN_ALL_TESTS();
 }
